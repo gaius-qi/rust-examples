271398d3639d83911aa3b4e73799b7e53396d614
Gaius <gaius.qi@gmail.com>
2023-08-06 16:39:17 +0800

    feat: ip
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- local-ip-address/src/main.rs
+++ local-ip-address/src/main.rs
@@ -4,6 +4,5 @@ fn main() {
     let local_ipv4 = local_ip().unwrap();
     let local_ipv6 = local_ipv6().unwrap();
 
-    println!("This is my local IPv4 address: {:?}", local_ipv4);
-    println!("This is my local IPv6 address: {:?}", local_ipv6);
+    println!("This is my local address: {:?} {:?}", local_ipv4, local_ipv6);
 }
