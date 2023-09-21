a0a568b9950e32997aedcf093d06a29f1e52e606
Gaius <gaius.qi@gmail.com>
2023-09-21 14:52:08 +0800

    feat: add closure
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- closure/src/main.rs
+++ closure/src/main.rs
@@ -1,20 +1,24 @@
 use sha2::{Digest, Sha256};
-use tokio::fs::File;
-use tokio::io;
+use tokio::fs::{File, OpenOptions};
+use tokio::io::{self, AsyncSeekExt, SeekFrom};
 use tokio_util::io::InspectReader;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let mut input = File::open("input").await.unwrap();
+    let input = File::open("src/input").await.unwrap();
 
     let mut hasher = Sha256::new();
-    let mut hashing = |bytes| hasher.update(bytes);
-    let tee = InspectReader::new(input, hashing);
+    let mut tee = InspectReader::new(input, |bytes| hasher.update(bytes));
 
-    let mut output = File::open("output").await.unwrap();
-    io::copy(tee.into_inner(), &mut output).await.unwrap();
+    let mut output = OpenOptions::new()
+        .write(true)
+        .open("src/output")
+        .await
+        .unwrap();
+    output.seek(SeekFrom::Start(3)).await.unwrap();
 
-    println!("{:x}", hasher.finalize());
+    io::copy(&mut tee, &mut output).await.unwrap();
+    println!("hash: {:x}", hasher.finalize());
     Ok(())
 }
 
