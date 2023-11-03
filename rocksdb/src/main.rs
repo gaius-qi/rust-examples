455c79ed7e1e95d2de53ea12be049605c679ab82
Gaius <gaius.qi@gmail.com>
2023-11-03 17:36:29 +0800

    feat: seek
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- rocksdb/src/main.rs
+++ rocksdb/src/main.rs
@@ -5,33 +5,23 @@ fn main() {
     let path = "./test";
     {
         let mut opts = Options::default();
-        opts.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(6));
         opts.create_if_missing(true);
+        opts.create_missing_column_families(true);
+        opts.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(3));
+        opts.set_memtable_prefix_bloom_ratio(0.2);
 
         let db = DB::open(&opts, path).unwrap();
-        db.put(b"piece-1", b"1").unwrap();
-        db.put(b"piece-2", b"2").unwrap();
-        db.put(b"piece-3", b"3").unwrap();
-        db.put(b"task-1", b"xxx-1").unwrap();
-        db.put(b"task-2", b"xxx-2").unwrap();
 
-        let mut iter = db.raw_iterator();
-        iter.seek(b"piece-");
-        while iter.valid() {
-            println!(
-                "Saw {:?} {:?}",
-                str::from_utf8(iter.key().unwrap()),
-                str::from_utf8(iter.value().unwrap())
-            );
-            iter.next();
-        }
+        db.put("aaa1".as_bytes(), b"aaa1").unwrap();
+        db.put("aaa2".as_bytes(), b"aaa2").unwrap();
+        db.put("bbb1".as_bytes(), b"bbb1").unwrap();
+        db.put("bbb2".as_bytes(), b"bbb2").unwrap();
 
-        match db.get(b"task") {
-            Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
-            Ok(None) => println!("value not found"),
-            Err(e) => println!("operational problem encountered: {}", e),
+        let iter = db.prefix_iterator("aaa".as_bytes());
+        for ele in iter {
+            println!("{:?}", str::from_utf8(&ele.unwrap().1).unwrap());
         }
-        db.delete(b"task").unwrap();
     }
+
     let _ = DB::destroy(&Options::default(), path);
 }
