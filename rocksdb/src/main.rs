1fa14b14056b3a0899b7ff2d0c1feebb1ef9c080
Gaius <gaius.qi@gmail.com>
2023-12-13 18:10:54 +0800

    feat: TransactionDB
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- rocksdb/src/main.rs
+++ rocksdb/src/main.rs
@@ -1,4 +1,4 @@
-use rocksdb::{Options, DB};
+use rocksdb::{Options, TransactionDB, TransactionDBOptions, DB};
 use std::str;
 
 fn main() {
@@ -18,14 +18,17 @@ fn main() {
         db.put("bbb2".as_bytes(), b"bbb2").unwrap();
         let iter = db.prefix_iterator("aaa".as_bytes());
         for ele in iter {
-            println!("{:?}", str::from_utf8(&ele.unwrap().0).unwrap());
-            println!("{:?}", str::from_utf8(&ele.unwrap().1).unwrap());
+            println!(
+                "test - key: {:?} value: {:?}",
+                str::from_utf8(&ele.clone().unwrap().0).unwrap(),
+                str::from_utf8(&ele.clone().unwrap().1).unwrap()
+            );
         }
     }
 
     let _ = DB::destroy(&Options::default(), path);
 
-    let path = "./test-2";
+    let path = "./test2";
     {
         let mut opts = Options::default();
         opts.create_if_missing(true);
@@ -51,8 +54,47 @@ fn main() {
         db.put_cf(handle, "bbb2".as_bytes(), b"bbb2").unwrap();
         let iter = db.prefix_iterator_cf(handle, "aaa".as_bytes());
         for ele in iter {
-            println!("{:?}", str::from_utf8(&ele.unwrap().0).unwrap());
-            println!("{:?}", str::from_utf8(&ele.unwrap().1).unwrap());
+            println!(
+                "test2 - key: {:?} value: {:?}",
+                str::from_utf8(&ele.clone().unwrap().0).unwrap(),
+                str::from_utf8(&ele.clone().unwrap().1).unwrap()
+            );
+        }
+    }
+
+    let _ = DB::destroy(&Options::default(), path);
+
+    let path = "./test3";
+    {
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        opts.create_missing_column_families(true);
+        opts.set_prefix_extractor(rocksdb::SliceTransform::create_fixed_prefix(3));
+        opts.set_memtable_prefix_bloom_ratio(0.2);
+
+        let db: TransactionDB =
+            TransactionDB::open(&opts, &TransactionDBOptions::default(), path).unwrap();
+
+        let txn = db.transaction();
+        txn.put("aaa1".as_bytes(), b"aaa1").unwrap();
+        txn.put("aaa2".as_bytes(), b"aaa2").unwrap();
+        txn.put("bbb1".as_bytes(), b"bbb1").unwrap();
+        txn.put("bbb2".as_bytes(), b"bbb2").unwrap();
+        let mut value = txn
+            .get_for_update("aaa1".as_bytes(), true)
+            .unwrap()
+            .unwrap();
+        value.pop().unwrap();
+        txn.put("aaa1".as_bytes(), value).unwrap();
+        txn.commit().unwrap();
+
+        let iter = db.prefix_iterator("aaa".as_bytes());
+        for ele in iter {
+            println!(
+                "test - key: {:?} value: {:?}",
+                str::from_utf8(&ele.clone().unwrap().0).unwrap(),
+                str::from_utf8(&ele.clone().unwrap().1).unwrap()
+            );
         }
     }
 
