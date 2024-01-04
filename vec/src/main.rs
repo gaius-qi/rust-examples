e479f3b525b77292808966c0d7c35114ea732b50
Gaius <gaius.qi@gmail.com>
2024-01-04 22:34:45 +0800

    feat: vec
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- vec/src/main.rs
+++ vec/src/main.rs
@@ -1,10 +1,14 @@
-#[derive(Debug)]
+use std::error::Error;
+use tokio::sync::RwLock;
+
+#[derive(Debug, Clone, PartialEq)]
 struct Person {
     name: String,
     age: u8,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let mut vals = Vec::new();
     vals.push(Person {
         name: String::from("John"),
@@ -36,4 +40,35 @@ fn main() {
     vals.retain(|val| !views.contains(&val.name));
 
     println!("Filtered by name: {:?}", vals);
+
+    let mut v1 = Vec::new();
+    let v2 = RwLock::new(Vec::new());
+
+    v1.push(Person {
+        name: String::from("John"),
+        age: 32,
+    });
+    v1.push(Person {
+        name: String::from("Adam"),
+        age: 34,
+    });
+
+    v2.write().await.push(Person {
+        name: String::from("John"),
+        age: 32,
+    });
+    v2.write().await.push(Person {
+        name: String::from("Adam"),
+        age: 34,
+    });
+
+    let v2 = v2.read().await;
+
+    if v1.len() == v2.len() && v1.iter().all(|e| v2.contains(e)) {
+        println!("v1 and v2 are equal");
+    } else {
+        println!("v1 and v2 are not equal");
+    }
+
+    Ok(())
 }
