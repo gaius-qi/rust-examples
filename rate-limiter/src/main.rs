e0fbdefb0f453389052a0a430db844fdb4ee6cb3
Gaius <gaius.qi@gmail.com>
2024-01-10 18:19:23 +0800

    feat: rate limiter
    Signed-off-by: Gaius <gaius.qi@gmail.com>

---
+++ rate-limiter/src/main.rs
@@ -0,0 +1,23 @@
+use leaky_bucket::RateLimiter;
+use std::time;
+
+#[tokio::main]
+async fn main() {
+    let limiter = RateLimiter::builder().max(10).initial(0).refill(5).build();
+
+    let start = time::Instant::now();
+
+    println!("Waiting for permit...");
+
+    // Should take ~400 ms to acquire in total.
+    let a = limiter.acquire(7);
+    let b = limiter.acquire(3);
+    let c = limiter.acquire(10);
+
+    let ((), (), ()) = tokio::join!(a, b, c);
+
+    println!(
+        "I made it in {:?}!",
+        time::Instant::now().duration_since(start)
+    );
+}
