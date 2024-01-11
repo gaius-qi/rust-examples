8b6c18c2393e33373a12ba972fd7b0902203ee4e
Gaius <gaius.qi@gmail.com>
2024-01-11 11:24:21 +0800

    feat: ratelimiter
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- rate-limiter/src/main.rs
+++ rate-limiter/src/main.rs
@@ -3,16 +3,21 @@ use std::time;
 
 #[tokio::main]
 async fn main() {
-    let limiter = RateLimiter::builder().max(10).initial(0).refill(5).build();
+    let limiter = RateLimiter::builder()
+        .initial(1024)
+        .refill(1024)
+        .interval(std::time::Duration::from_secs(1))
+        .fair(false)
+        .build();
 
     let start = time::Instant::now();
 
     println!("Waiting for permit...");
 
     // Should take ~400 ms to acquire in total.
-    let a = limiter.acquire(7);
-    let b = limiter.acquire(3);
-    let c = limiter.acquire(10);
+    let a = limiter.acquire(1024);
+    let b = limiter.acquire(1024);
+    let c = limiter.acquire(1024);
 
     let ((), (), ()) = tokio::join!(a, b, c);
 
