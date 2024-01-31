17fd37a5f584097d65cab7714f4d8f936ebe260d
Gaius <gaius.qi@gmail.com>
2024-01-31 17:11:39 +0800

    feat: proxy
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,65 +1,73 @@
-use hyper::service::make_service_fn;
-use hyper::service::service_fn;
-use hyper::{Body, Client, Request, Response, Server};
 use std::net::SocketAddr;
 
+use bytes::Bytes;
+use futures_util::TryStreamExt;
+use http_body_util::{combinators::BoxBody, BodyExt, StreamBody};
+use hyper::body::Frame;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response, StatusCode};
+use hyper_util::rt::tokio::TokioIo;
+use tokio::io::AsyncWriteExt;
+use tokio_util::io::ReaderStream;
+
+use tokio::net::TcpListener;
+
+// To try this example:
+// 1. cargo run --example http_proxy
+// 2. config http_proxy in command line
+//    $ export http_proxy=http://127.0.0.1:8100
+//    $ export https_proxy=http://127.0.0.1:8100
+// 3. send requests
+//    $ curl -i https://www.some_domain.com/
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    start_proxy_server().await?;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8100));
 
-    Ok(())
-}
-
-async fn proxy_service(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
-    println!(
-        "Got request at: {} {:?} {:?} {:?} {:?}",
-        req.uri().to_string(),
-        req.uri().host(),
-        req.uri().port(),
-        req.uri().path(),
-        req.uri().query(),
-    );
-
-    let mut parts = req.uri().clone().into_parts();
-    parts.scheme = Some(http::uri::Scheme::HTTPS);
-
-    let new_uri = http::Uri::from_parts(parts).unwrap();
-    println!("New URI: {}", new_uri);
-
-    let client = Client::new();
-    println!("Forwarding request to: {} {}", new_uri, req.method());
-
-    let forwarded_req = Request::builder()
-        .method(req.method())
-        .uri(new_uri)
-        .version(req.version())
-        .body(req.into_body())
-        .unwrap();
-
-    client.request(forwarded_req).await
-}
-
-async fn start_proxy_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // load the certificates
-    // let certs = load_certs("path_to_your_cert.pem")?;
-    // let key = load_private_key("path_to_your_key.pem")?;
-
-    // let mut config = ServerConfig::new(NoClientAuth::new());
-    // config.set_single_cert(certs, key)?;
-
-    // let acceptor = TlsAcceptor::from(Arc::new(config));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-
-    let make_svc =
-        make_service_fn(|_conn| async { Ok::<_, hyper::Error>(service_fn(proxy_service)) });
-
-    // let server = Server::bind(&addr).tls(acceptor).serve(make_svc);
-    let server = Server::bind(&addr).serve(make_svc);
-
+    let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
 
-    if let Err(e) = server.await {
-        eprintln!("proxy server error: {}", e);
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .preserve_header_case(true)
+                .title_case_headers(true)
+                .serve_connection(io, service_fn(proxy))
+                .with_upgrades()
+                .await
+            {
+                println!("Failed to serve connection: {:?}", err);
+            }
+        });
     }
-    Ok(())
+}
+
+async fn proxy(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, std::io::Error>>, std::io::Error> {
+    println!("req: {:?}", req);
+
+    // Write the task data to the reader.
+    let (reader, mut writer) = tokio::io::duplex(1024);
+    writer.write_all(b"Hello World!").await?;
+
+    writer.shutdown().await?;
+
+    // Wrap to a tokio_util::io::ReaderStream
+    let reader_stream = ReaderStream::new(reader);
+
+    // Convert to http_body_util::BoxBody
+    let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
+    let boxed_body = stream_body.boxed();
+
+    // Send response
+    let response = Response::builder()
+        .status(StatusCode::OK)
+        .body(boxed_body)
+        .unwrap();
+
+    Ok(response)
 }
