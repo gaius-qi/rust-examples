1fc0346fff6b8ed5f872ed4dd505a0598ae9572f
Gaius <gaius.qi@gmail.com>
2024-02-26 22:11:45 +0800

    feat: proxy
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -1,28 +1,19 @@
 use std::net::SocketAddr;
 
 use bytes::Bytes;
-use futures_util::TryStreamExt;
-use http_body_util::{combinators::BoxBody, BodyExt, StreamBody};
-use hyper::body::Frame;
+use http_body_util::{combinators::BoxBody, BodyExt, Empty, Full};
+use hyper::client::conn::http1::Builder;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
-use hyper::{Request, Response, StatusCode};
+use hyper::upgrade::Upgraded;
+use hyper::{Method, Request, Response};
+
 use hyper_util::rt::tokio::TokioIo;
-use tokio::io::AsyncWriteExt;
-use tokio_util::io::ReaderStream;
+use tokio::net::{TcpListener, TcpStream};
 
-use tokio::net::TcpListener;
-
-// To try this example:
-// 1. cargo run --example http_proxy
-// 2. config http_proxy in command line
-//    $ export http_proxy=http://127.0.0.1:8100
-//    $ export https_proxy=http://127.0.0.1:8100
-// 3. send requests
-//    $ curl -i https://www.some_domain.com/
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8100));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8100));
 
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
@@ -47,27 +38,125 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 async fn proxy(
     req: Request<hyper::body::Incoming>,
-) -> Result<Response<BoxBody<Bytes, std::io::Error>>, std::io::Error> {
-    println!("req: {:?}", req);
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    println!("1----------------req: {:?}", req);
+    println!(
+        "2----------------authorization: {:?}",
+        req.headers().get("Authorization")
+    );
 
-    // Write the task data to the reader.
-    let (reader, mut writer) = tokio::io::duplex(1024);
-    writer.write_all(b"Hello World!").await?;
+    if Method::CONNECT == req.method() {
+        // Received an HTTP request like:
+        // ```
+        // CONNECT www.domain.com:443 HTTP/1.1
+        // Host: www.domain.com:443
+        // Proxy-Connection: Keep-Alive
+        // ```
+        //
+        // When HTTP method is CONNECT we should return an empty body
+        // then we can eventually upgrade the connection and talk a new protocol.
+        //
+        // Note: only after client received an empty body with STATUS_OK can the
+        // connection be upgraded, so we can't return a response inside
+        // `on_upgrade` future.
+        if let Some(addr) = host_addr(req.uri()) {
+            tokio::task::spawn(async move {
+                match hyper::upgrade::on(req).await {
+                    Ok(upgraded) => {
+                        if let Err(e) = tunnel(upgraded, addr).await {
+                            eprintln!("server io error: {}", e);
+                        };
+                    }
+                    Err(e) => eprintln!("upgrade error: {}", e),
+                }
+            });
 
-    writer.shutdown().await?;
+            Ok(Response::new(empty()))
+        } else {
+            eprintln!("CONNECT host is not socket addr: {:?}", req.uri());
+            let mut resp = Response::new(full("CONNECT must be to a socket address"));
+            *resp.status_mut() = http::StatusCode::BAD_REQUEST;
 
-    // Wrap to a tokio_util::io::ReaderStream
-    let reader_stream = ReaderStream::new(reader);
+            Ok(resp)
+        }
+    } else {
+        let host = "index.docker.io";
+        let port = req.uri().port_u16().unwrap_or(80);
 
-    // Convert to http_body_util::BoxBody
-    let stream_body = StreamBody::new(reader_stream.map_ok(Frame::data));
-    let boxed_body = stream_body.boxed();
+        let mut new_req = Request::builder()
+            .uri(format!("https://index.docker.io{}", req.uri().path()))
+            .header("Host", "index.docker.io")
+            .method(req.method())
+            .body(Empty::<Bytes>::new())
+            .unwrap();
 
-    // Send response
-    let response = Response::builder()
-        .status(StatusCode::OK)
-        .body(boxed_body)
-        .unwrap();
+        *new_req.headers_mut() = req.headers().clone();
+        new_req
+            .headers_mut()
+            .insert("Host", "index.docker.io".parse().unwrap())
+            .unwrap();
 
-    Ok(response)
+        let stream = TcpStream::connect((host, port)).await.unwrap();
+        let io = TokioIo::new(stream);
+
+        let (mut sender, conn) = Builder::new()
+            .preserve_header_case(true)
+            .title_case_headers(true)
+            .handshake(io)
+            .await?;
+        tokio::task::spawn(async move {
+            if let Err(err) = conn.await {
+                println!("Connection failed: {:?}", err);
+            }
+        });
+
+        println!("2-------------req: {:?}", new_req);
+        println!("2-------------scheme: {:?}", new_req.uri().scheme());
+        println!("2-------------host: {:?}", new_req.uri().host());
+        println!(
+            "2-------------authorization: {:?}",
+            new_req.headers().get("Authorization")
+        );
+
+        let resp = sender.send_request(new_req).await?;
+        println!("3-------------resp: {:?}", resp);
+        println!("3-------------status: {:?}", resp.status());
+        Ok(resp.map(|b| b.boxed()))
+    }
+}
+
+fn host_addr(uri: &http::Uri) -> Option<String> {
+    uri.authority().and_then(|auth| Some(auth.to_string()))
+}
+
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
+}
+
+// Create a TCP connection to host:port, build a tunnel between the connection and
+// the upgraded connection
+async fn tunnel(upgraded: Upgraded, addr: String) -> std::io::Result<()> {
+    // Connect to remote server
+    let mut server = TcpStream::connect(addr).await?;
+    let mut upgraded = TokioIo::new(upgraded);
+
+    // Proxying data
+    let (from_client, from_server) =
+        tokio::io::copy_bidirectional(&mut upgraded, &mut server).await?;
+
+    // Print message when done
+    println!(
+        "client wrote {} bytes and received {} bytes",
+        from_client, from_server
+    );
+
+    Ok(())
 }
