52aa262101ee8d25fa000c43b116d29232e75f92
Gaius <gaius.qi@gmail.com>
2024-04-16 11:04:27 +0800

    feat: scheme
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- url/src/main.rs
+++ url/src/main.rs
@@ -3,8 +3,9 @@ use url::Url;
 fn main() {
     let url = Url::parse("http://github.com:8080").unwrap();
     println!(
-        "{}:{}",
+        "{}:{} - {}",
         url.host_str().unwrap(),
-        url.port_or_known_default().unwrap()
+        url.port_or_known_default().unwrap(),
+        url.scheme(),
     );
 }
