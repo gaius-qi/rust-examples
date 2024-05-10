34f64d9480362ad7136d82fdc94f68e66d3b3d03
Gaius <gaius.qi@gmail.com>
2024-05-10 12:11:47 +0800

    feat: blake3
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hashes/src/main.rs
+++ hashes/src/main.rs
@@ -6,5 +6,15 @@ fn main() {
     hasher.update("foo");
     let hash = hasher.finalize();
 
-    println!("hash: {:?}", hex::encode(hash));
+    println!("sha256 hash: {:?}", hex::encode(hash));
+
+    let mut hasher1 = blake3::Hasher::new();
+    hasher1.update("127.0.0.1".as_bytes());
+    hasher1.update("foo".as_bytes());
+    let hash1 = hasher1.finalize();
+
+    println!(
+        "blake3 hash: {:?}",
+        base16ct::lower::encode_string(hash1.as_bytes())
+    );
 }
