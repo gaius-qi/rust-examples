938425bb9da476af4d2cb8a3e8fe841c79e82ed1
Gaius <gaius.qi@gmail.com>
2025-01-17 16:10:21 +0800

    feat: crc32
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- crc/src/main.rs
+++ crc/src/main.rs
@@ -1,3 +1,4 @@
+use crc::*;
 use crc32fast::Hasher;
 
 fn main() {
@@ -13,4 +14,14 @@ fn main() {
     const BYTES: &[u8] = "Hello world!".as_bytes();
     const CKSUM: u32 = const_crc32::crc32(BYTES);
     println!("hash = {}", CKSUM.to_string());
+
+    let crc = Crc::<u32>::new(&CRC_32_ISCSI);
+    let mut digest = crc.digest();
+    digest.update(b"Hello world!");
+    println!("hash = {}", digest.finalize().to_string());
+
+    let crc = Crc::<u32, Table<16>>::new(&CRC_32_ISCSI);
+    let mut digest = crc.digest();
+    digest.update(b"Hello world!");
+    println!("hash = {}", digest.finalize().to_string());
 }
