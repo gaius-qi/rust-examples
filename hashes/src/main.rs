aff846d00c864da6473581581360851b86328caf
Gaius <gaius.qi@gmail.com>
2025-01-17 16:42:39 +0800

    feat: xxh3
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- hashes/src/main.rs
+++ hashes/src/main.rs
@@ -1,4 +1,5 @@
 use sha2::{Digest, Sha256};
+use xxhash_rust::xxh3::Xxh3;
 
 fn main() {
     let mut hasher = Sha256::new();
@@ -12,10 +13,19 @@ fn main() {
     let mut hasher1 = blake3::Hasher::new();
     hasher1.update("127.0.0.1".as_bytes());
     hasher1.update("foo".as_bytes());
+    hasher1.update("bar".as_bytes());
     let hash1 = hasher1.finalize();
 
     println!(
         "blake3 hash: {:?}",
         base16ct::lower::encode_string(hash1.as_bytes())
     );
+
+    let mut hasher2 = Xxh3::new();
+    hasher2.update("https://example.com".as_bytes());
+    hasher2.update("foo".as_bytes());
+    hasher2.update("bar".as_bytes());
+    let hash2 = hasher2.digest().to_string();
+
+    println!("xxh3 hash: {:?}", hash2);
 }
