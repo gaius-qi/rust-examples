163d8e2dd83fee9aca14602f9173c3bd5cc804b2
Gaius <gaius.qi@gmail.com>
2025-06-18 20:34:38 +0800

    feat: sysinfo
    Signed-off-by: Gaius <gaius.qi@gmail.com>

--- sysinfo/src/main.rs
+++ sysinfo/src/main.rs
@@ -1,30 +1,69 @@
-use sysinfo::Networks;
+use procfs;
+use std::{
+    thread::{self, sleep},
+    time,
+};
+use sysinfo::{self};
 
 fn main() {
-    let mut networks = Networks::new_with_refreshed_list();
-    let network_data = networks.get("en0").unwrap();
-    println!("Network transmit speed: {}", network_data.transmitted());
-    println!(
-        "Network total bytes transmitted: {}",
-        network_data.total_transmitted()
-    );
-    println!("Network receive speed: {}", network_data.received());
-    println!(
-        "Network total bytes received: {}",
-        network_data.total_received()
-    );
+    // let mut networks = Networks::new_with_refreshed_list();
+    // let network_data = networks.get("en0").unwrap();
+    // println!("Network transmit speed: {}", network_data.transmitted());
+    // println!(
+    // "Network total bytes transmitted: {}",
+    // network_data.total_transmitted()
+    // );
+    // println!("Network receive speed: {}", network_data.received());
+    // println!(
+    // "Network total bytes received: {}",
+    // network_data.total_received()
+    // );
 
-    std::thread::sleep(std::time::Duration::from_secs(5));
-    networks.refresh();
-    let network_data = networks.get("en0").unwrap();
-    println!("Network transmit speed: {}", network_data.transmitted());
-    println!(
-        "Network total bytes transmitted: {}",
-        network_data.total_transmitted()
-    );
-    println!("Network receive speed: {}", network_data.received());
-    println!(
-        "Network total bytes received: {}",
-        network_data.total_received()
-    );
+    // std::thread::sleep(std::time::Duration::from_secs(5));
+    // networks.refresh();
+    // let network_data = networks.get("en0").unwrap();
+    // println!("Network transmit speed: {}", network_data.transmitted());
+    // println!(
+    // "Network total bytes transmitted: {}",
+    // network_data.total_transmitted()
+    // );
+    // println!("Network receive speed: {}", network_data.received());
+    // println!(
+    // "Network total bytes received: {}",
+    // network_data.total_received()
+    // );
+
+    let pid = sysinfo::get_current_pid().unwrap();
+    println!("I am: {:}", pid);
+    let mut system = sysinfo::System::new_all();
+
+    for i in 0..10 {
+        let children: Vec<_> = (0..1000)
+            .map(|_| {
+                thread::spawn(move || {
+                    // just having a bunch of threads doing nothing
+                    sleep(time::Duration::from_secs(5));
+                })
+            })
+            .collect();
+
+        if !system.refresh_processes(pid, true) {
+            println!("Process refreshing failed");
+            break;
+        }
+
+        let process = procfs::process::Process::new(pid).expect("Our process exists. qed.");
+        let threads = process.stat().ok().map(|s| s.num_threads).unwrap();
+        let open_fd = process.fd().map(|f| f.len()).unwrap();
+
+        println!(
+            "Iteration {:}: I have {:} threads and {:} open file handles",
+            i, threads, open_fd
+        );
+
+        for t in children {
+            t.join().unwrap();
+        }
+        // these threads are all gone now.
+    }
 }
